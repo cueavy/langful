c0e61b3ba7b9f2462dc93dac873d9c05661b0dca
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-06-20 23:56:16 +0800

    updata

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open( "README.md" , "r" , encoding = "utf-8" ) as fh :
     long_description = fh.read()
 setuptools.setup(
     name="langful" , # 模块名称
-    version="0.27" , # 当前版本
+    version="0.28" , # 当前版本
     author="cueavyqwp" , # 作者
     author_email="cueavyqwp@outlook.com" , # 作者邮箱
     description="", # 模块简介
