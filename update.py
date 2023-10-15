7ad11664e921af232e99bcf48360865fe8a203cd
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-10-15 16:56:10 +0800

    change `update.py` file

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+import tkinter.messagebox
 import tkinter.ttk
 import subprocess
 import tkinter
@@ -21,21 +22,35 @@ class window :
 
     def init( self ) -> None :
         ( frame := tkinter.Frame( self.root ) ).pack( fill = "both" , expand = True )
-
-        tkinter.ttk.Checkbutton(frame).pack()
-
+        self.entry = tkinter.ttk.Entry( frame )
+        self.entry.pack( fill = "x" )
+        self.checkbuttons = []
+        for value in [ "build" , "release" ] :
+            tkinter.Label( f := tkinter.Frame( frame ) , text = value ).pack( side = "left" , fill = "x" )
+            checkbutton = tkinter.ttk.Checkbutton( f , variable = ( i := tkinter.BooleanVar() ) )
+            checkbutton.pack( side = "right" , fill = "x" )
+            self.checkbuttons.append( i )
+            f.pack( expand = True )
         tkinter.ttk.Separator( self.root ).pack( fill = "x" )
-        tkinter.ttk.Button( self.root , text = "update" ).pack()
+        tkinter.ttk.Button( self.root , text = "update" , command = self.update ).pack()
 
     def run( self ) -> None :
         self.init()
         self.root.mainloop()
 
-    def update( self , commit : str , build : bool , release : bool ) -> None :
+    def update( self ) -> None :
+        commit = self.entry.get()
+        build , release = [ i.get() for i in self.checkbuttons ]
+        try :
+            assert commit , "no commint"
+            assert all( tkinter.messagebox.askokcancel( "update" , f"are you sure to update ( update after { _ } times )" ) for _ in range( 5 , 0 , -1 ) ) , "user cancel"
+        except AssertionError as e :
+            tkinter.messagebox.showwarning( "cancel" , e )
+            return
         subprocess.check_call( [ "git" , "add" , "." ] )
         subprocess.check_call( [ "git" , "commit" , "-m" , commit ] )
         subprocess.check_call( [ "git" , "push" , "origin" ] )
-        subprocess.check_call( [ sys.executable , "pack.py" ] ) if build else None
+        subprocess.check_call( [ sys.executable , "pack.py" ] ) if build or release else None
         if release :
             subprocess.check_call( [ "git" , "tag" , version ] )
             subprocess.check_call( [ "git" , "push" , "origin" , "--tags" ] )
