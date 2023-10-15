26f673e7a7f06742e02fe64abfd0ebcd77e2e3cb
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2023-10-15 16:08:14 +0800

    test

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
+import tkinter.ttk
 import subprocess
+import tkinter
 import sys
 import os
 
@@ -10,32 +12,35 @@ version = __import__( name ).__version__
 
 if len ( i := version.split( "." ) ) < 3 : version = ".".join( i + list( value for value in i.pop( -1 ) ) )
 
-for count in range( 3 , 0 , -1 ) :
-    try :
-        print( f"{name} { version }\n" )
-        assert ( commit := input( "commit\n> " ) )
-        build = input( "\nbuild[Y/n](N)\n> " ).lower()
-        build = True if build in [ "y" , "yes" ] else False
-        if build :
-            release = input( "\nrelease[Y/n](Y)\n> " ).lower()
-            release = True if release in [ "n" , "no" ] else False
-        else :
-            release = False
-        assert all( input( f"\nbe sure to update[Y/n](Y) ( update after { i - 1} )\n> " ).lower() not in [ "n" , "no" ] for i in range( 3 , 0 , -1 ) )
-        input( f"\n{name} { version }\ncommit { commit }\nbuild { build }\nrelease { release }\n" )
-    except ( KeyboardInterrupt , EOFError , AssertionError ) :
-        print( f"input error try again (quit after { count - 1 } times)" )
-    else :
-        break
-else :
-    print( "exit" )
-    exit()
+class window :
 
-subprocess.check_call( [ "git" , "add" , "." ] )
-subprocess.check_call( [ "git" , "commit" , "-m" , commit ] )
-subprocess.check_call( [ "git" , "push" , "origin" ] )
-subprocess.check_call( [ sys.executable , "pack.py" ] ) if build else None
-if release :
-    subprocess.check_call( [ "git" , "tag" , version ] )
-    subprocess.check_call( [ "git" , "push" , "origin" , "--tags" ] )
-    subprocess.check_call( [ sys.executable , "-m" , "twine" , "upload" , "dist/*" ] )
+    def __init__( self ) -> None :
+        self.root = tkinter.Tk()
+        self.root.title( "update" )
+        self.root.geometry( "240x100" )
+
+    def init( self ) -> None :
+        ( frame := tkinter.Frame( self.root ) ).pack( fill = "both" , expand = True )
+
+        tkinter.ttk.Checkbutton(frame).pack()
+
+        tkinter.ttk.Separator( self.root ).pack( fill = "x" )
+        tkinter.ttk.Button( self.root , text = "update" ).pack()
+
+    def run( self ) -> None :
+        self.init()
+        self.root.mainloop()
+
+    def update( self , commit : str , build : bool , release : bool ) -> None :
+        subprocess.check_call( [ "git" , "add" , "." ] )
+        subprocess.check_call( [ "git" , "commit" , "-m" , commit ] )
+        subprocess.check_call( [ "git" , "push" , "origin" ] )
+        subprocess.check_call( [ sys.executable , "pack.py" ] ) if build else None
+        if release :
+            subprocess.check_call( [ "git" , "tag" , version ] )
+            subprocess.check_call( [ "git" , "push" , "origin" , "--tags" ] )
+            subprocess.check_call( [ sys.executable , "-m" , "twine" , "upload" , "dist/*" ] )
+
+if __name__ == "__main__" :
+    root = window()
+    root.run()
