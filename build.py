a64037ea5561aa115778078e04545ec9e91252f6
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2024-03-17 21:20:18 +0800

    change workflows

---
+++ build.py
@@ -0,0 +1,23 @@
+"""
+test and build
+"""
+import subprocess
+import shutil
+import json
+import sys
+import os
+# install & update modules
+subprocess.check_call( [ sys.executable , "-m" , "pip" , "install" , "--upgrade" , "pip" , "toml" , "twine" , "build" , "pytest" ] )
+import pytest
+import toml
+# test
+os.chdir( os.path.dirname( __file__ ) )
+sys.path.insert( 0 , "src" )
+assert pytest.main( [ "-s" ] ) == 0
+# update pyproject.toml
+with open( "pyproject.json" , "r" , encoding = "utf-8" ) as file : data = json.load( file )
+data[ "project" ][ "version" ] = __import__( data[ "project" ][ "name" ] ).__version__
+with open( "pyproject.toml" , "w" , encoding = "utf-8" ) as file : toml.dump( data , file )
+# build
+if os.path.exists( "dist" ) : shutil.rmtree( "dist" )
+subprocess.check_call( [ sys.executable , "-m" , "build" ] )
