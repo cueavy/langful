b0d41bb53624f42104817e229d5f8f93092ccc21
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-01-24 20:05:28 +0800

    update the `pack.py`

--- pack.py
+++ pack.py
@@ -1,23 +1,27 @@
 """
 test and build
 """
+
 import subprocess
 import shutil
 import json
 import sys
 import os
-# install & update modules
-subprocess.check_call( [ sys.executable , "-m" , "pip" , "install" , "--upgrade" , "build" , "pytest" , "toml" ] )
-import pytest
+
 import toml
-# test
-os.chdir( os.path.dirname( __file__ ) )
+
 sys.path.insert( 0 , "src" )
-assert pytest.main( [ "-s" ] ) == 0
+os.environ[ "PYTHONPATH" ] = "src"
+os.chdir( os.path.dirname( __file__ ) )
+subprocess.check_call( ( sys.executable , "-m" , "pytest" , "-s" ) )
+
 # update pyproject.toml
-with open( "pyproject.json" , "r" , encoding = "utf-8" ) as file : data = json.load( file )
+with open( "pyproject.json" , "r" , encoding = "utf-8" ) as file :
+    data = json.load( file )
 data[ "project" ][ "version" ] = __import__( data[ "project" ][ "name" ] ).__version__
-with open( "pyproject.toml" , "w" , encoding = "utf-8" ) as file : toml.dump( data , file )
-# build
-if os.path.exists( "dist" ) : shutil.rmtree( "dist" )
-subprocess.check_call( [ sys.executable , "-m" , "build" ] )
+with open( "pyproject.toml" , "w" , encoding = "utf-8" ) as file :
+    toml.dump( data , file )
+
+if os.path.exists( "dist" ) :
+    shutil.rmtree( "dist" )
+subprocess.check_call( ( sys.executable , "-m" , "build" ) )
