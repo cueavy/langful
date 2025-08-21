30bce4d881a67149dfb9c113bb5585d9f95ccb2b
cueavyqwp <127200270+cueavyqwp@users.noreply.github.com>
2025-08-21 12:16:03 +0800

    update `pack.py`

--- pack.py
+++ pack.py
@@ -14,7 +14,7 @@ sys.path.insert(0, "src")
 os.environ["PYTHONPATH"] = "src"
 os.chdir(os.path.dirname(__file__))
 subprocess.check_call((sys.executable, "-m", "pytest", "-s"))
-exit()
+
 # update pyproject.toml
 with open("pyproject.json", "r", encoding="utf-8") as file:
     data = json.load(file)
